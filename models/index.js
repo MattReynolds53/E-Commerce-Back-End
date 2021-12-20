26ff297d4b72e44ce4929044ec014b637e93fd31
Matt Reynolds <mattreynolds53@gmail.com>
2021-12-20 13:02:24 -0500

    Added relations to index.js on models page for the belongToMany and hasMany sections

--- models/index.js
+++ models/index.js
@@ -11,10 +11,28 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignkey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_info'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
